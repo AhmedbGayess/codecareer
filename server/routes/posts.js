a0b95e2895a24bdb6adf54089dc8d850936f7950
AhmedbGayess <38194881+AhmedbGayess@users.noreply.github.com>
2020-01-28 14:27:11 +0100

    delete comment

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -106,4 +106,41 @@ router.post("/comment/:id", auth, async (req, res) => {
   }
 });
 
+// Remove comment from post
+router.delete("/comment/:post_id/:comment_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+
+    if (!post) {
+      return res.status(404).send("No post found");
+    }
+
+    const commentToDelete = post.comments.find(
+      (comm) => comm._id.toString() === req.params.comment_id
+    );
+
+    if (!commentToDelete) {
+      return res.status(404).send("No comment found");
+    }
+
+    if (
+      commentToDelete.user.toString() !== req.user.id &&
+      post.user.toString() !== req.user.id
+    ) {
+      return res.status(401).send("Unauthorized");
+    }
+
+    const filteredComments = post.comments.filter(
+      (comment) => comment._id.toString() !== commentToDelete._id.toString()
+    );
+
+    post.comments = filteredComments;
+
+    await post.save();
+    res.send(post);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
 module.exports = router;
