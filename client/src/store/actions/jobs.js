57fe9d6f85e7d7c7a07635d738fe1295b7ca7585
AhmedbGayess <38194881+AhmedbGayess@users.noreply.github.com>
2020-02-02 15:00:25 +0100

    get jobs and delete job actions

--- client/src/store/actions/jobs.js
+++ client/src/store/actions/jobs.js
@@ -48,6 +48,43 @@ export const editJob = (id, jobData, history) => async () => {
   }
 };
 
+export const deleteJob = (id, history) => async () => {
+  try {
+    await axios.delete(`/api/jobs/${id}`);
+    history.push("/home");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const getJobs = (skip, search) => async (dispatch) => {
+  dispatch(setJobLoading());
+  try {
+    const { data } = await axios.get(
+      `/api/jobs?skip=${skip}}&search=${search}`
+    );
+    dispatch({
+      type: SET_JOBS,
+      payload: data
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const getOwnJobs = (skip) => async (dispatch) => {
+  dispatch(setJobLoading());
+  try {
+    const { data } = await axios.get(`/api/jobs/own-jobs?skip=${skip}}`);
+    dispatch({
+      type: SET_JOBS,
+      payload: data
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 const setJobLoading = () => ({
   type: SET_JOB_LOADING
 });
